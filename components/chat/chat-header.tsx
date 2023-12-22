909beca1b65ad66d8e399164bebf952eb7c86532
morelzy <technicalzfg@gmail.com>
2023-12-22 12:10:27 -0400

    conversation set up

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import { Hash } from 'lucide-react'
 
 import { MobileToggle } from '@/components/mobile-toggle'
+import { UserAvatar } from '@/components/user-avatar'
 
 interface ChatHeaderProps {
   serverId: string
@@ -20,6 +21,9 @@ export function ChatHeader({
       <MobileToggle serverId={serverId} />
       {type === 'channel' && (
         <Hash className="mr-2 h-5 w-5 text-zinc-500 dark:text-zinc-400" />
+      )}
+      {type === 'conversation' && (
+        <UserAvatar className="mr-2 h-8 w-8 md:h-8 md:w-8" src={imageUrl} />
       )}
       <p>{name}</p>
     </div>
